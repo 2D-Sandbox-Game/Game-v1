6feb662e060bbd61af7c92374c83eb0e7fe1a5d8
erkelentziszi84569 <75340926+erkelentziszi84569@users.noreply.github.com>
2021-05-17 14:54:03 +0200

    deleted redundant script (playerMine)

--- game_v1/Assets/Scripts/PlayerMine.cs
+++ game_v1/Assets/Scripts/PlayerMine.cs
@@ -2,103 +2,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
 
 public class PlayerMine : MonoBehaviour
 {
     public Tilemap tilemap;
-    public Tile inputTile;
-    public Animator animator;
     public GameObject blockBreaking;
+    public GameObject pickaxe;
+    public GameObject selector;
+    public ItemDatabaseObject database;
+    public GameObject createableItem;
+
+    Animator breakingAnim;
+    public float miningSpeed = 3;
+    float miningDuration;
+
+    Animator pickaxeAnim;
+
+    TileBase selectedTile = null;
+    Vector3 mousePos = Vector3.zero;
+    Vector3Int mousePosTranslated = Vector3Int.zero;
+    Vector3Int posSelectedTile = Vector3Int.zero;
+    float timeSinceMiningStart;
+
+    float mx;
+    float prevMx = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        breakingAnim = blockBreaking.GetComponent<Animator>();
+        pickaxeAnim = pickaxe.GetComponent<Animator>();
+        breakingAnim.speed = miningSpeed;
+        miningDuration = 1 / miningSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*  Vector3 mousePos;
-          TileData tileData;
-          Vector3Int mousePosInt;
-          GameObject block;
-          Color col = Color.blue;
-          MeshRenderer renderer;
-          if (Input.GetKey(KeyCode.T))
-          {
-              mousePos = Input.mousePosition;
-              mousePosInt = new Vector3Int((int)mousePos.x, (int)mousePos.y, (int)mousePos.z);
-             // TileBase tile = tilemap.GetTile(mousePosInt);
-              tilemap.DeleteCells(mousePosInt)
-              tilemap.SetTile(mousePosInt, inputTile);
-              //renderer = block.GetComponent<MeshRenderer>();
-              //renderer.material.color = col;
-              Debug.Log("Test");
-          }
+        //World Space
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Mouse Position On tile Map
+        //Converts worlds space click point to tile map click point.
+        mousePosTranslated = tilemap.WorldToCell(mousePos);
+        selectedTile = tilemap.GetTile(mousePosTranslated);
 
-          */
-
-        Vector3 mousePos = Vector3.zero;
-        Vector3Int mousePosTranslated = Vector3Int.zero;
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (mousePosTranslated != posSelectedTile)
         {
-            Debug.Log("Click");
+            timeSinceMiningStart = 0;
 
-            //World Space
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selector.GetComponent<SpriteRenderer>().color = Color.clear;
+            posSelectedTile = mousePosTranslated;
 
-            //Mouse Position On tile Map
-            //Converts worlds space click point to tile map click point.
-            mousePosTranslated = tilemap.WorldToCell(mousePos);
-
-            //PAINTS BOXES
-            //activeTileMap.BoxFill(mousePosTranslated, gameManager.cobbleTile, mousePosTranslated.x,mousePosTranslated.y,mousePosTranslated.x +1,mousePosTranslated.y +1);
-
-            //GETS TILE NAME AND PRINTS IT
-            //                Debug.Log( activeTileMap.GetTile(mousePosTranslated));
-            //                Debug.Log( gameManager.objectsMap.GetTile(mousePosTranslated));
-            //                Debug.Log( gameManager.bordersMap.GetTile(mousePosTranslated));
-
-
-            //now we can change tiles! :slight_smile:
-
-            TileBase clickedTile = null;
-            ITilemap itilemap = null;
-            TileData tileData = new TileData();
-
-
-            clickedTile = tilemap.GetTile(mousePosTranslated);
-
-            //CHANGES TILES THAT ARE CLICKED ON
-            if (clickedTile == null)
-                tilemap.SetTile(mousePosTranslated, inputTile);
-            else
+            if (selectedTile != null)
             {
-                animator.SetBool("Mining", true);
-
-                //Start the coroutine we define below named ExampleCoroutine.
-                StartCoroutine(ExampleCoroutine());
-                blockBreaking.transform.position = new Vector3(0.5f + mousePosTranslated.x, 0.5f + mousePosTranslated.y, 10);
-
-
+                selector.transform.position = new Vector3(0.5f + posSelectedTile.x, 0.5f + posSelectedTile.y);
+                selector.GetComponent<SpriteRenderer>().color = Color.white;
             }
-            // clickedTile.GetTileData(mousePosTranslated, itilemap, ref tileData);
-
         }
-        IEnumerator ExampleCoroutine()
+
+        if (selectedTile != null)
         {
-            //Print the time of when the function is first called.
-            Debug.Log("Started Coroutine at timestamp : " + Time.time);
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                blockBreaking.transform.position = new Vector3(0.5f + posSelectedTile.x, 0.5f + posSelectedTile.y);
+                breakingAnim.Play("BlockBreaking");
+                pickaxeAnim.Play("Swinging");
 
-            //yield on a new YieldInstruction that waits for 5 seconds.
-            yield return new WaitForSeconds(2);
+                timeSinceMiningStart += Time.deltaTime;
 
-            tilemap.SetTile(mousePosTranslated, null);
-            animator.SetBool("Mining", false);
+                if (timeSinceMiningStart > miningDuration)
+                {
+                    breakingAnim.Play("Idle");
+                    pickaxeAnim.Play("Idle");
+                    tilemap.SetTile(posSelectedTile, null);
+                    timeSinceMiningStart = 0;
+                    GenerateItem(selectedTile.name);
+                }
+            }
+        }
 
-            //After we have waited 5 seconds print the time again.
-            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        if (Input.GetKeyUp(KeyCode.Mouse0) || selectedTile == null)
+        {
+            breakingAnim.Play("Idle");
+            pickaxeAnim.Play("Idle");
+            timeSinceMiningStart = 0;
+        }
+
+        mx = Input.GetAxisRaw("Horizontal");
+
+        //if (mx != prevMx && mx != 0)
+        //{
+        //    if (mx == -1)
+        //    {
+        //        pickaxe.GetComponent<SpriteRenderer>().flipX = true;
+        //        pickaxe.transform.position += new Vector3(-1, 0);
+        //    }
+        //    else
+        //    {
+        //        pickaxe.GetComponent<SpriteRenderer>().flipX = false;
+        //        pickaxe.transform.position += new Vector3(1, 0);
+        //    }
+
+        //    Debug.Log($"mx: {mx}");
+
+        //    prevMx = mx;
+        //}
+    }
+    void GenerateItem(string name)
+    {
+        for (int i = 0; i < database.Items.Length; i++)
+        {
+            if (name.Contains("dirt2") || name.Contains("grass")) //just a temporary fix, need to find a way to convert the name to an ID with various names for one ID
+            {
+                name = "Dirt";
+            }
+            if (name == database.Items[i].name)
+            {
+                createableItem.GetComponent<GroundItem>().item = database.Items[i];
+                Instantiate(createableItem, posSelectedTile + new Vector3(0.5f,0.5f,0), Quaternion.identity);
+                //createableItem.GetComponent<Rigidbody2D>().AddForce(new Vector2(20, 0),ForceMode2D.Impulse); attempt to give the items an inital velocity when they spawn
+            }
         }
     }
 }
+

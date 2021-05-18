c65a907e8ba9be89f8ab916858e993c5cc5c48f9
erkelentziszi84569 <75340926+erkelentziszi84569@users.noreply.github.com>
2021-05-18 16:57:07 +0200

    renamed files

--- game_v1/Assets/Scripts/PlayerMovement2.cs
+++ game_v1/Assets/Scripts/PlayerMovement2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement2 : MonoBehaviour
 {
     bool runFaster = false;
     float curentSpeed = 1;

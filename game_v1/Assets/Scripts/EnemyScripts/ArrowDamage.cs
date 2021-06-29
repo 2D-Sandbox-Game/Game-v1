1bf2a8cd2a6bfde86179cf4b68cbb47bf897f9e6
Ronja151 <82866489+Ronja151@users.noreply.github.com>
2021-06-29 17:59:15 +0200

    Arrowdamage

---
+++ game_v1/Assets/Scripts/EnemyScripts/ArrowDamage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowDamage : MonoBehaviour
+{
+    public int damageGeist = 1;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+                collision.gameObject.SendMessage("HealthDamage", -damageGeist, SendMessageOptions.DontRequireReceiver);
+            else
+                collision.gameObject.SendMessage("HealthDamage", damageGeist, SendMessageOptions.DontRequireReceiver);
+
+        }
+    }
+
+}
